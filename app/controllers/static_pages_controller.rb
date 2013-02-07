89c2b8dd6ebd075027e6b3e51f25d084e78a80ab
drewskione <jurinka@yahoo.com>
2013-02-06 18:45:47 -0800

    Finish layout and routes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,12 +3,18 @@ class StaticPagesController < ApplicationController
   end
 
   def help
+  	#help_path => "/help"
+	#help_url  => "http://localhost:3000/help"
   end
 
   def about
+  	#about_path => "/about"
+	#about_url  => "http://localhost:3000/about"
   end
 
   def contact
+  	#contact_path => "/contact"
+	#contact_url  => "http://localhost:3000/contact"
   end
 
 end
